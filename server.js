33b3a7f27d0c3c54869818ade3c4bd99ee10929f
GottemukkalaGayathri <gayathrigottemukkala123@gmail.com>
2026-05-05 17:02:43 +0530

    my ch

--- server.js
+++ server.js
@@ -48,15 +48,11 @@ app.use("/api/invoices", invoiceRoutes);
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
-
-
-
-
 // Serve frontend dist
 app.use(express.static(path.join(__dirname, "dist")));
 
 // React/Vite fallback route
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
