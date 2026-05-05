8e6a7476d7e4164bcc9a58e838d4355f49f73d97
GottemukkalaGayathri <gayathrigottemukkala123@gmail.com>
2026-05-05 17:40:41 +0530

    ch

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 // Load environment variables immediately
-dotenv.config();
-
 const path = require("path");
+dotenv.config({ path: path.join(__dirname, ".env") });
 const userRoutes = require("./routes/Userroutes");
 const authRoutes = require("./routes/Authroutes");
 const leaveRoutes = require("./routes/Leavesroutes");
